8a0277e38d3260cf4f7be2de9075c9a60830e055
faishalshidqi <rshidqi6@gmail.com>
2025-02-22 22:35:35 +0700

    added home page action and add note button

---
+++ src/components/AddNoteButton.tsx
@@ -0,0 +1,5 @@
+export default function AddNoteButton() {
+    return (
+        <p>TODO:</p>
+    )
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import NotesList from "../components/NotesList.tsx";
 import {Component} from "react";
 import {getActiveNotes} from "../utils/data.ts";
+import AddNoteButton from "../components/AddNoteButton.tsx";
 
 export default class HomePage extends Component<unknown, { notes: { id: string, title: string, body: string, createdAt: string, archived: boolean }[] }> {
     constructor(props: unknown) {
@@ -15,6 +16,9 @@ export default class HomePage extends Component<unknown, { notes: { id: string, 
             <section className="homepage">
                 <h2>Active Notes</h2>
                 <NotesList notes={this.state.notes}/>
+                <div className="homepage__action">
+                    <AddNoteButton/>
+                </div>
             </section>
         )
     }
