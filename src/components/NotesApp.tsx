d3748fa2a69167a20c5247f9ff7f93037166e607
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 00:01:43 +0700

    added note detail page

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -2,6 +2,7 @@ import NotesAppHeader from "./NotesAppHeader.tsx";
 import HomePage from "../pages/HomePage.tsx";
 import {Route, Routes} from "react-router-dom";
 import ArchivePage from "../pages/ArchivePage.tsx";
+import DetailPageWrapper from "../pages/NoteDetailPage.tsx";
 
 export default function NotesApp() {
     return (
@@ -11,7 +12,7 @@ export default function NotesApp() {
                 <Routes>
                     <Route path='/' element={<HomePage/>} />
                     <Route path='/archives' element={<ArchivePage/>} />
-                    <Route path='/notes/:id' element={<ArchivePage/>} />
+                    <Route path='/notes/:id' element={<DetailPageWrapper/>} />
                 </Routes>
             </main>
         </div>
---
+++ src/pages/NoteDetailPage.tsx
@@ -0,0 +1,25 @@
+import NoteDetail from "../components/NoteDetail.tsx";
+import {Component} from "react";
+import {getNote} from "../utils/data.ts"
+import {useParams} from "react-router-dom";
+
+export default function DetailPageWrapper() {
+    const {id} = useParams()
+    return <DetailPage id={id ?? ''}/>
+}
+
+class DetailPage extends Component<{id: string}, {note: {id: string, title: string, body: string, createdAt: string, archived: boolean}|undefined}> {
+    constructor(props: {id: string}) {
+        super(props)
+        this.state = {
+            note: getNote(props.id)
+        }
+    }
+    render() {
+        return (
+            <section>
+                <NoteDetail title={this.state.note?.title ?? ''} body={this.state.note?.body ?? ''} createdAt={this.state.note?.createdAt ?? ''} />
+            </section>
+        )
+    }
+}
