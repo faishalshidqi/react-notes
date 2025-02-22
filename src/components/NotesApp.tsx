3d55f6dcae29ec085938086e11a2162e7fbd9379
faishalshidqi <rshidqi6@gmail.com>
2025-02-22 22:49:35 +0700

    added root route bound to home page

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -1,12 +1,15 @@
 import NotesAppHeader from "./NotesAppHeader.tsx";
 import HomePage from "../pages/HomePage.tsx";
+import {Route, Routes} from "react-router-dom";
 
 export default function NotesApp() {
     return (
         <div className='app-container'>
             <NotesAppHeader />
             <main>
-                <HomePage/>
+                <Routes>
+                    <Route path='/' element={<HomePage/>} />
+                </Routes>
             </main>
         </div>
     )
