ff39751916ad4051167e086fd43222e63c9ff75a
faishalshidqi <rshidqi6@gmail.com>
2025-02-22 22:19:32 +0700

    added notes state to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,21 @@
-export default function HomePage() {
-    return (
-        <section className="homepage">
-            <h2>Active Notes</h2>
-            TODO: add note list
-        </section>
-    )
+import NotesList from "../components/NotesList.tsx";
+import {Component} from "react";
+import {getActiveNotes} from "../utils/data.ts";
+
+export default class HomePage extends Component<unknown, { notes: { id: string, title: string, body: string, createdAt: string, archived: boolean }[] }> {
+    constructor(props: unknown) {
+        super(props);
+        this.state = {
+            notes: getActiveNotes()
+        }
+    }
+
+    render() {
+        return (
+            <section className="homepage">
+                <h2>Active Notes</h2>
+                <NotesList notes={this.state.notes}/>
+            </section>
+        )
+    }
 }
