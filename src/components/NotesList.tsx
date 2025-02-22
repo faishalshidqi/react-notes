616cdff09681c7868c2203ebb485ab4678dcad34
faishalshidqi <rshidqi6@gmail.com>
2025-02-22 23:42:52 +0700

    updated notes list to show message if notes state is empty

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,6 +1,9 @@
 import NoteItem from "./NoteItem.tsx";
 
 export default function NotesList({notes}: {notes: {id: string, title: string, body: string, createdAt: string, archived: boolean}[]}) {
+    if (notes.length === 0) {
+        return <section className='notes-list-empty'><p>There are no notes here!</p></section>
+    }
     return (
         <section className="notes-list">
             {
