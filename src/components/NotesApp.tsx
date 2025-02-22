79e0fc48e70b35a986161fb0720552018c2de2c4
faishalshidqi <rshidqi6@gmail.com>
2025-02-22 21:53:05 +0700

    added home page TODO

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -1,10 +1,12 @@
 import NotesAppHeader from "./NotesAppHeader.tsx";
+import HomePage from "../pages/HomePage.tsx";
 
 export default function NotesApp() {
     return (
         <div className='app-container'>
             <NotesAppHeader />
             <main>
+                <HomePage/>
             </main>
         </div>
     )
