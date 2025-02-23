eea9fe43a47d2513d9d216ef5515c28fbdd4ad06
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 09:22:38 +0700

    added send button component

---
+++ src/components/SendButton.tsx
@@ -0,0 +1,9 @@
+import {IoMdCheckmark} from "react-icons/io";
+
+export default function SendButton() {
+    return (
+        <button type="button" className='action' title='Send'>
+            <IoMdCheckmark/>
+        </button>
+    )
+}
