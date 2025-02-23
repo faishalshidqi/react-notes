774154aba6aec1a55a50a0796891f23461a2b958
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 09:22:31 +0700

    added send button component

--- src/components/NoteInput.tsx
+++ src/components/NoteInput.tsx
@@ -1,9 +1,13 @@
+import SendButton from "./SendButton.tsx";
+
 export default function NoteInput() {
     return (
         <>
             <div className='add-new-page__input'>
                 <input className='add-new-page__input__title' placeholder='Judul Catatan' />
-                <div className='add-new-page__input__body' data-placeholder='Judul Catatan' contentEditable>
+                <div className='add-new-page__input__body' data-placeholder='Judul Catatan' contentEditable></div>
+                <div className='add-new-page__action'>
+                    <SendButton/>
                 </div>
             </div>
         </>
