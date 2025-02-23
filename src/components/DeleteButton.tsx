6cbf7eba2c45f5ebb7d88e868b419a8472530f28
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 08:41:10 +0700

    updated delete button icon

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,7 +1,7 @@
-import {FaTrash} from "react-icons/fa";
+import {FiTrash} from "react-icons/fi";
 
 export default function DeleteButton({id}: {id: string}) {
     return (
-        <button type="button" className='action' title='Delete'><FaTrash/></button>
+        <button type="button" className='action' title='Delete'><FiTrash/></button>
     )
 }
