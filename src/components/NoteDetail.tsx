a53e5278cad716bf6c37634ed85c38082c09f270
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 15:51:51 +0700

    render formatted date in note detail page

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,11 +1,12 @@
 import DeleteButton from "./DeleteButton.tsx";
 import ArchiveButton from "./ArchiveButton.tsx";
+import {showFormattedDate} from "../utils/data.ts";
 
 export default function NoteDetail({id, title, body, createdAt, archived, onDelete, onArchive}: {id: string, title: string, body: string, createdAt: string, archived: boolean, onDelete: (id: string) => void, onArchive: (id: string) => void}) {
     return (
         <>
             <h3 className='detail-page__title'>{title}</h3>
-            <p className='detail-page__createdAt'>{createdAt}</p>
+            <p className='detail-page__createdAt'>{showFormattedDate(createdAt)}</p>
             <div className='detail-page__body'>{body}</div>
             <div className='detail-page__action'>
                 <ArchiveButton id={id} onArchive={onArchive} archived={archived} />
