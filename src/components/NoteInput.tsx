b0c0aafa82838abc1877f17e2d6d69302f942e61
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 09:16:36 +0700

    added note input consisting of title and body input

--- src/components/NoteInput.tsx
+++ src/components/NoteInput.tsx
@@ -2,7 +2,9 @@ export default function NoteInput() {
     return (
         <>
             <div className='add-new-page__input'>
-
+                <input className='add-new-page__input__title' placeholder='Judul Catatan' />
+                <div className='add-new-page__input__body' data-placeholder='Judul Catatan' contentEditable>
+                </div>
             </div>
         </>
     )
