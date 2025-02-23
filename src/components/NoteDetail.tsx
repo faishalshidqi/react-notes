d07d44733d8f2338173927a46338980bdf61508b
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 08:37:18 +0700

    added id property to NoteDetail component

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -4,6 +4,9 @@ export default function NoteDetail({title, body, createdAt}: {title: string, bod
             <h3 className='detail-page__title'>{title}</h3>
             <p className='detail-page__createdAt'>{createdAt}</p>
             <div className='detail-page__body'>{body}</div>
+            <div className='detail-page__action'>
+                <DeleteButton id={id}/>
+            </div>
         </>
     )
 }
