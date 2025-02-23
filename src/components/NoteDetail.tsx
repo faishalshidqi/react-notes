61ab33641e84c32af33040e514f7b8adbcb1e597
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 08:45:06 +0700

    added archived property to note detail for archive button conditional rendering

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,14 +1,14 @@
 import DeleteButton from "./DeleteButton.tsx";
 import ArchiveButton from "./ArchiveButton.tsx";
 
-export default function NoteDetail({id, title, body, createdAt}: {id: string, title: string, body: string, createdAt: string}) {
+export default function NoteDetail({id, title, body, createdAt, archived}: {id: string, title: string, body: string, createdAt: string, archived: boolean}) {
     return (
         <>
             <h3 className='detail-page__title'>{title}</h3>
             <p className='detail-page__createdAt'>{createdAt}</p>
             <div className='detail-page__body'>{body}</div>
             <div className='detail-page__action'>
-                <ArchiveButton id={id}/>
+                <ArchiveButton id={id} archived={archived} />
                 <DeleteButton id={id}/>
             </div>
         </>
