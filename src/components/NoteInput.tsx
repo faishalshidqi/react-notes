70d41fd77f6a075a31136e5c87436a414202c34a
faishalshidqi <rshidqi6@gmail.com>
2025-02-23 18:27:55 +0700

    added add note functionality at /notes/new

--- src/components/NoteInput.tsx
+++ src/components/NoteInput.tsx
@@ -1,15 +1,44 @@
 import SendButton from "./SendButton.tsx"
+import {Component} from "react"
 
-export default function NoteInput() {
-    return (
-        <>
-            <div className='add-new-page__input'>
-                <input className='add-new-page__input__title' placeholder='Judul Catatan' />
-                <div className='add-new-page__input__body' data-placeholder='Konten Catatan' contentEditable></div>
-                <div className='add-new-page__action'>
-                    <SendButton/>
+export default class NoteInput extends Component<{addNote: ({title, body}: {title: string, body: string}) => void}, {id: string, title: string, body: string, createdAt: string, archived: boolean}> {
+    constructor(props: { addNote: ({ title, body }: { title: string; body: string }) => void }) {
+        super(props)
+        this.state = {
+            id: '',
+            title: '',
+            body: '',
+            createdAt: '',
+            archived: false,
+        }
+        this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this)
+        this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onTitleChangeEventHandler = (event: { target: { value: string} }) => {
+        if (event.target.value.length <= 50) {
+            this.setState(() => ({title: event.target.value}))
+        }
+    }
+    onBodyChangeEventHandler = (event: { target: { innerHTML: string } }) => {
+        this.setState(() => ({body: event.target.innerHTML}))
+    }
+    onSubmit = (event: {preventDefault: () => void}) => {
+        event.preventDefault()
+        this.props.addNote(this.state)
+        this.setState(() => ({id: '', title: '', body: '', createdAt: '', archived: false}))
+    }
+    render() {
+        return (
+            <form>
+                <div className='add-new-page__input'>
+                    <input className='add-new-page__input__title' placeholder='Judul Catatan'/>
+                    <div className='add-new-page__input__body' data-placeholder='Konten Catatan' contentEditable></div>
+                    <div className='add-new-page__action'>
+                        <SendButton/>
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+            </form>
+        )
+    }
 }
--- src/pages/AddNotePage.tsx
+++ src/pages/AddNotePage.tsx
@@ -1,9 +1,23 @@
 import NoteInput from "../components/NoteInput.tsx"
+import {Component} from "react"
+import {addNote} from "../utils/data.ts";
 
-export default function AddNotePage() {
-    return (
-        <div className='add-new-page'>
-            <NoteInput />
-        </div>
-    )
+export default class AddNotePage extends Component<unknown, unknown> {
+    constructor(props: unknown) {
+        super(props)
+        this.onAddNoteHandler = this.onAddNoteHandler.bind(this)
+    }
+    onAddNoteHandler = ({title, body}: {title: string, body: string}): void => {
+        this.setState(() => {
+            addNote({title, body})
+        })
+    }
+    render() {
+        return (
+            <div className='add-new-page'>
+                <h2>Add Note</h2>
+                <NoteInput addNote={this.onAddNoteHandler} />
+            </div>
+        )
+    }
 }
