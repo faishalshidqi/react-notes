0084cb5d8b45d587a33e35d46f3f34ab25c576fc
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 04:21:06 +0700

    added locale context

--- src/contexts/LocaleContext.ts
+++ src/contexts/LocaleContext.ts
@@ -1,6 +1,6 @@
 import React from "react"
 
-const LocaleContext = React.createContext({locale: 'id', toggleLocale: () => {}})
+const LocaleContext = React.createContext({locale: 'en', toggleLocale: () => {}})
 export const LocaleProvider = LocaleContext.Provider
 export const LocaleConsumer = LocaleContext.Consumer
 
