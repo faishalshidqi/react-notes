eb11bdec3aac789764db224f9d5911c9069c5cf9
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 04:30:15 +0700

    added route group when user is not authenticated, to login page and route page

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -5,8 +5,28 @@ import ArchivePage from '../pages/ArchivePage.tsx'
 import DetailPageWrapper from '../pages/NoteDetailPage.tsx'
 import AddNotePage from '../pages/AddNotePage.tsx'
 import NotFoundPage from '../pages/NotFoundPage.tsx'
+import {useState} from 'react'
+import LoginPage from '../pages/LoginPage.tsx'
+import RegisterPage from '../pages/RegisterPage.tsx'
 
 export default function NotesApp() {
+    const [user, setUser] = useState<{id: string, name: string, email: string}>({id: '', name: '', email: ''})
+
+    if (user.id === '') {
+        return (
+            <div className='app-container'>
+                <NotesAppHeader />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<LoginPage/>} />
+                        <Route path='/register' element={<RegisterPage/>} />
+                        <Route path='*' element={<NotFoundPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        )
+    }
+
     return (
         <div className='app-container'>
             <NotesAppHeader />
