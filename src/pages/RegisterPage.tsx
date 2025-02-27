6d304446e908213d0865566e1162af6d2137fc5f
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 22:52:01 +0700

    added id translation to register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,19 +1,29 @@
 import RegisterInput from '../components/RegisterInput'
 import {Link, useNavigate} from 'react-router-dom'
 import {register} from '../utils/data.ts'
+import {useContext} from 'react'
+import LocaleContext from "../contexts/LocaleContext.ts";
 
 export default function RegisterPage() {
+    const localeContext = useContext(LocaleContext)
     const navigate = useNavigate()
     async function onRegisterHandler({name, email, password}: {name: string, email: string, password: string}) {
         const {error} = await register({name, email, password})
         if (!error) navigate('/')
     }
 
-    return (
-        <section className={'register-page'}>
-            <h2>Fill the form to register an account</h2>
-            <RegisterInput onRegister={onRegisterHandler} />
-            <p>Already have an account? <Link to={'/'}>Login Here</Link></p>
-        </section>
-    )
+    function ret({heading, linkQuestion, linkMessage}: {heading: string, linkQuestion: string, linkMessage: string}) {
+        return (
+            <section className={'register-page'}>
+                <h2>{heading}</h2>
+                <RegisterInput onRegister={onRegisterHandler} />
+                <p>{linkQuestion} <Link to={'/'}>{linkMessage}</Link></p>
+            </section>
+        )
+    }
+
+    const enRet = ret({heading: 'Fill the form to register an account!', linkQuestion: 'Already have an account?', linkMessage: 'Login Here'})
+    const idRet = ret({heading: 'Isi form berikut untuk mendaftarkan akun!', linkQuestion: 'Sudah punya akun?', linkMessage: 'Login Di Sini'})
+
+    return localeContext.locale === 'en' ? enRet : idRet
 }
