569ccbe7d100be6424875a6324671a6d13b0ce1f
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 22:58:30 +0700

    added id translation to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,26 @@
 import {login} from '../utils/data.ts'
 import {Link} from 'react-router-dom'
 import LoginInput from '../components/LoginInput'
+import LocaleContext from '../contexts/LocaleContext.ts'
+import {useContext} from 'react'
 
 export default function LoginPage({onSuccessLogin}: {onSuccessLogin: ({accessToken}: {accessToken: string}) => void}) {
+    const localeContext = useContext(LocaleContext)
     async function onLoginHandler({email, password}: {email: string, password: string}) {
         const {error, data} = await login({email, password})
         if (!error) onSuccessLogin(data)
     }
-    return (
-        <section className={'login-page'}>
-            <h2>You must login first to enter this app!</h2>
-            <LoginInput onLogin={onLoginHandler} />
-            <p>Doesn't have an account yet? <Link to={'/register'}>Register Here</Link></p>
-        </section>
-    )
+    function returnComponent({heading, linkQuestion, linkMessage}: {heading: string, linkQuestion: string, linkMessage: string}) {
+        return (
+            <section className={'login-page'}>
+                <h2>{heading}</h2>
+                <LoginInput onLogin={onLoginHandler} />
+                <p>{linkQuestion} <Link to={'/register'}>{linkMessage}</Link></p>
+            </section>
+        )
+    }
+    const enRet = returnComponent({heading: 'You must login first to enter this app!', linkQuestion: 'Doesn\'t have an account yet?', linkMessage: 'Register Here'})
+    const idRet = returnComponent({heading: 'Login dulu ya!', linkQuestion: 'Belum punya akun?', linkMessage: 'Daftar Di Sini'})
+
+    return localeContext.locale === 'en' ? enRet : idRet
 }
