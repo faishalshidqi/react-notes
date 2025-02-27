5facffa204d921f8c575267d415e78f634a6eacd
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 17:16:55 +0700

    updated notes app header to use user auth context for conditional rendering

--- src/components/NotesAppHeader.tsx
+++ src/components/NotesAppHeader.tsx
@@ -1,13 +1,24 @@
 import Navigation from './Navigation.tsx'
 import {Link} from 'react-router-dom'
+import {useContext} from 'react'
+import UserAuthContext from '../contexts/UserAuthContext.ts'
 
-export default function NotesAppHeader() {
-    return (
+export default function NotesAppHeader({onLogout}: {onLogout: () => void}) {
+    const userContext = useContext(UserAuthContext);
+    const not_authed = (
         <header>
             <h1>
                 <Link to='/'>Notes App</Link>
             </h1>
-            <Navigation/>
         </header>
     )
+    const authed = (
+        <header>
+            <h1>
+                <Link to='/'>Notes App</Link>
+            </h1>
+            <Navigation onLogout={onLogout} />
+        </header>
+    )
+    return userContext.id === '' ? not_authed : authed
 }
