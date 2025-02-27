26ee0d38951c009219d5f7b9c45a12940b7f2ebb
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 17:17:33 +0700

    updated notes app to use user auth context for conditional rendering

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -5,18 +5,29 @@ import ArchivePage from '../pages/ArchivePage.tsx'
 import DetailPageWrapper from '../pages/NoteDetailPage.tsx'
 import AddNotePage from '../pages/AddNotePage.tsx'
 import NotFoundPage from '../pages/NotFoundPage.tsx'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useMemo} from 'react'
 import LoginPage from '../pages/LoginPage.tsx'
 import RegisterPage from '../pages/RegisterPage.tsx'
-import {getUserLogged, putAccessToken} from "../utils/data.ts";
+import {getUserLogged, putAccessToken} from '../utils/data.ts'
+import UserAuthContext from '../contexts/UserAuthContext.ts'
 
 export default function NotesApp() {
     const [user, setUser] = useState<{id: string, name: string, email: string}>({id: '', name: '', email: ''})
+    const userAuthContextValue = useMemo(() => {
+        return {
+            user
+        }
+    }, [user])
     async function onSuccessLogin({accessToken}: {accessToken: string}) {
         putAccessToken(accessToken)
         const {data} = await getUserLogged()
         setUser(data)
     }
+    const onLogout = () => {
+        setUser({id: '', name: '', email: ''})
+        putAccessToken('')
+    }
+
     useEffect(() => {
         getUserLogged().then(({error, data}) => {
             if (error) (setUser({id: '', name: '', email: ''}))
@@ -25,31 +36,35 @@ export default function NotesApp() {
     }, [])
 
     const not_authed = (
-        <div className='app-container'>
-            <NotesAppHeader />
-            <main>
-                <Routes>
-                    <Route path='/' element={<LoginPage onSuccessLogin={onSuccessLogin}/>} />
-                    <Route path='/register' element={<RegisterPage/>} />
-                    <Route path='*' element={<NotFoundPage/>}/>
-                </Routes>
-            </main>
-        </div>
+        <UserAuthContext.Provider value={userAuthContextValue.user}>
+            <div className='app-container'>
+                <NotesAppHeader onLogout={onLogout} />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<LoginPage onSuccessLogin={onSuccessLogin}/>} />
+                        <Route path='/register' element={<RegisterPage/>} />
+                        <Route path='*' element={<NotFoundPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </UserAuthContext.Provider>
     )
 
     const authed = (
-        <div className='app-container'>
-            <NotesAppHeader />
-            <main>
-                <Routes>
-                    <Route path='/' element={<HomePage/>} />
-                    <Route path='/archives' element={<ArchivePage/>} />
-                    <Route path='/notes/:id' element={<DetailPageWrapper/>} />
-                    <Route path='/notes/new' element={<AddNotePage/>}/>
-                    <Route path='*' element={<NotFoundPage/>}/>
-                </Routes>
-            </main>
-        </div>
+        <UserAuthContext.Provider value={user}>
+            <div className='app-container'>
+                <NotesAppHeader onLogout={onLogout} />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<HomePage/>} />
+                        <Route path='/archives' element={<ArchivePage/>} />
+                        <Route path='/notes/:id' element={<DetailPageWrapper/>} />
+                        <Route path='/notes/new' element={<AddNotePage/>}/>
+                        <Route path='*' element={<NotFoundPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </UserAuthContext.Provider>
     )
 
     return user.id === '' ? not_authed : authed
