1b43dd9ef0c7fec7a75cd2b169a54a95bbd24db7
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 22:38:50 +0700

    added loading component to notes app component

--- src/components/NotesApp.tsx
+++ src/components/NotesApp.tsx
@@ -64,7 +64,7 @@ export default function NotesApp() {
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme)
-    }, [theme]);
+    }, [theme])
 
     const not_authed = (
         <ThemeProvider value={themeContextValue}>
