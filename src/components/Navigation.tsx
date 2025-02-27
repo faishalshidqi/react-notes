2f331ded79d68de2784389a2d99297dfe5ecd741
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 16:59:39 +0700

    added logout button to navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,15 @@
 import {Link} from 'react-router-dom'
+import {FaArchive} from "react-icons/fa";
+import {FiLogOut} from "react-icons/fi";
 
-export default function Navigation() {
+export default function Navigation({onLogout}: {onLogout: () => void}) {
     return <nav className='navigation'>
         <ul>
             <li>
-                <Link to='/archives'>Archive</Link>
+                <Link to='/archives'><FaArchive/></Link>
+            </li>
+            <li>
+                <Link to={'/'} onClick={onLogout}><FiLogOut/></Link>
             </li>
         </ul>
     </nav>
