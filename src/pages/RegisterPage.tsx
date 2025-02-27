d6449df584dcf45def5d304e0569b19764feb290
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 14:46:16 +0700

    updated register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,5 +1,19 @@
+import RegisterInput from '../components/RegisterInput'
+import {Link, useNavigate} from 'react-router-dom'
+import {register} from '../utils/data.ts'
+
 export default function RegisterPage() {
+    const navigate = useNavigate()
+    async function onRegisterHandler({name, email, password}: {name: string, email: string, password: string}) {
+        const {error} = await register({name, email, password})
+        if (!error) navigate('/')
+    }
+
     return (
-        <p>Register Page</p>
+        <section className={'register-page'}>
+            <h2>Fill the form to register an account</h2>
+            <RegisterInput onRegister={onRegisterHandler} />
+            <p>Already have an account? <Link to={'/'}>Login Here</Link></p>
+        </section>
     )
 }
