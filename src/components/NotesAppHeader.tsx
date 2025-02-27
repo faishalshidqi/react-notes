2faabfa8711b112aaaef875597ab5d96d0775a9a
faishalshidqi <rshidqi6@gmail.com>
2025-02-27 22:32:22 +0700

    updated notes app header component with locale context conditional rendering

--- src/components/NotesAppHeader.tsx
+++ src/components/NotesAppHeader.tsx
@@ -1,25 +1,16 @@
 import Navigation from './Navigation.tsx'
 import {Link} from 'react-router-dom'
 import {useContext} from 'react'
-import UserAuthContext from '../contexts/UserAuthContext.ts'
+import LocaleContext from '../contexts/LocaleContext.ts'
 
 export default function NotesAppHeader({onLogout}: {onLogout: () => void}) {
-    const userContext = useContext(UserAuthContext);
-    const not_authed = (
+    const localeContext = useContext(LocaleContext)
+    return (
         <header>
             <h1>
-                <Link to='/'>Notes App</Link>
+                <Link to='/'>{localeContext.locale === 'en' ? 'Notes App' : 'Aplikasi Catatan'}</Link>
             </h1>
             <Navigation onLogout={onLogout} />
         </header>
     )
-    const authed = (
-        <header>
-            <h1>
-                <Link to='/'>Notes App</Link>
-            </h1>
-            <Navigation onLogout={onLogout} />
-        </header>
-    )
-    return userContext.id === '' ? not_authed : authed
 }
